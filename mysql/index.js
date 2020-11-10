02c4ff5d2b2c6e6024cea7cc2f3fd4a96d6de892
lishuaibing <1440955662@qq.com>
2020-11-11 01:56:03 +0800

    1

--- mysql/index.js
+++ mysql/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /* 
     数据库基础配置
     host 地址
@@ -7,28 +6,9 @@
     password 密码
     database   数据库名
     connectTimeout: 连接超时
-*/
-const mysql = {
-    host: "localhost",
-    port: "3306",//可选，默认式3306
-    user: "root",
-    password: "123456",
-    database: "blog",
-    connectTimeout: 5000
-}
 
 
-
-
-=======
-/* 
-    数据库基础配置
-    host 地址
-    port 端口号
-    user 用户名
-    password 密码
-    database   数据库名
-    connectTimeout: 连接超时
+    
 */
 const mysql = {
     host: "localhost",
@@ -42,5 +22,4 @@ const mysql = {
 
 
 
->>>>>>> 4a16e97511dd04ba26592847429a6dc5a910c98e
 module.exports = mysql
